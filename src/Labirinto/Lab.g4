74a99840e02cf6de15d82435078c36fac536967e
mariana <marianaandrade@ua.pt>
2022-06-16 15:49:55 +0100

    NEW: gramatica e main da linguagem secundaria

---
+++ src/Labirinto/Lab.g4
@@ -0,0 +1,42 @@
+grammar Lab;
+
+program:
+    (stat? ';') EOF
+    ;
+
+stat:labirinto  
+    |grid
+    |labirinto ';' grid          
+    ;
+
+grid: 'GRID' '{' (position ';')* '}';
+position: 'POS' coordenadas ('DIRECTION' '=' INT)?; 
+
+labirinto: 'LABIRINTO' '(' 'NAME' '=' STRING 'WIDTH' '=' INT 'HEIGHT' '=' INT')' '{'
+                (dlab ';')*
+            '}' 
+    ;
+
+dlab: target
+    |beacon
+    |spot
+    |row
+    ;
+
+beacon:'BEACON' '=' coordenadas ('HEIGHT' '=' INT)?; 
+target:'TARGET' '=' coordenadas ('RADIUS' '=' (num))?;
+
+spot: 'SPOT' '=' coordenadas ('HEIGHT' '=' INT)? ('RADIUS' '=' (num))?;
+row :'ROW' INT ':' PADRAO;
+
+
+coordenadas: '(' num ',' num ')' ;
+num:(DOUBLE|INT);
+INT:('-')?[0-9]+;
+DOUBLE:('-')?([0-9]+ '.')? [0-9]+;
+STRING: '"'[a-zA-Z_][a-zA-Z_0-9]* '"';
+ID: [a-zA-Z_][a-zA-Z_0-9]* ;
+PADRAO: '"' (' ' |'|'|'+'|'--')* '"';
+WS: [ \t\r\n]+ -> skip;
+LINE_COMMENT: '#' .*? ('\n'|EOF) -> skip;
+MULTILINE_COMMENT: '/*' .*? '*/' -> skip;
---
+++ src/Labirinto/LabMain.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <antlr4-runtime.h>
+#include "LabLexer.h"
+#include "LabParser.h"
+#include "Execute.h"
+
+#include <fstream>
+
+using namespace std;
+using namespace antlr4;
+using namespace antlr4::tree;
+
+int main(int argc, const char* argv[]) {
+   
+   std::filebuf fb;
+   fb.open("teste.lab", std::ios::in);
+   if (!fb.is_open()){
+      cout << "File not open";
+      return -1;
+   }
+
+   std::istream stream(&fb);
+   // create a ANTLRInputStream that reads from standard input:
+   ANTLRInputStream* input;
+   //stream = &cin;
+   
+
+   input = new ANTLRInputStream(stream);
+   // create a lexer that feeds off of input ANTLRInputStream:
+   LabLexer* lexer = new LabLexer(input);
+   // create a buffer of tokens pulled from the lexer:
+   CommonTokenStream* tokens = new CommonTokenStream(lexer);
+   // create a parser that feeds off the tokens buffer:
+   LabParser* parser = new LabParser(tokens);
+   // replace error listener:
+   //parser->removeErrorListeners(); // remove ConsoleErrorListener
+   //parser->addErrorListener(new ErrorHandlingListener());
+   // begin parsing at program rule:
+   ParseTree* tree = parser->program();
+   if (parser->getNumberOfSyntaxErrors() == 0) {
+      // print LISP-style tree:
+      // cout << tree->toStringTree(parser) << endl;
+      Execute* visitor0 = new Execute();
+      visitor0->visit(tree);
+   }else{
+      cout<<"<< ERROS sintáticos\n\n";
+   }
+
+   fb.close();
+   return 0;
+}
